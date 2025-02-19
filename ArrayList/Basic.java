97eda4f8d19a5eb77ef1ae1c9a95603690e82c31
Amit Kumar <kumaramit20204@gmail.com>
2025-02-19 23:40:29 +0530

    added new file

---
+++ ArrayList/Basic.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.util.ArrayList;
+
+
+public class Basic{
+
+
+    public static void main(String args[])
+    {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+
+
+        System.out.print(list);
+    }
+
+
+}
