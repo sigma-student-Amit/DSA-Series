3cde67c9b26c4bbcfdfcd30b6310413bbbe50a1c
Amit Kumar <kumaramit20204@gmail.com>
2025-03-01 22:34:42 +0530

    Find the power of N

---
+++ Recursion/PowerofN.java
@@ -0,0 +1,22 @@
+public class GCD {
+
+    public static int power(int x, int n)
+    {
+
+        if(n == 0)
+        {
+            return 1;
+        }
+        int xnm1 = power(x, n-1);
+        int xn = x * xnm1;
+        return xn;
+    }
+
+
+
+
+    public static void main(String args[])
+    {
+        System.out.print(power(2, 4));
+    }
+}
